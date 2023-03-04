48c056e89de841295d51022863cb8632aed97373
pdostonh <pdostonh@gmail.com>
2023-03-05 00:53:48 +0500

    add button to start the app and change number of sides

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
+let sideNum = 20;
+let btnSide = document.createElement('button');
+btnSide.innerText = "sides count";
+btnSide.addEventListener('click', () => {
+    console.log('buttonFunc is working');
+    let sideNumAsk = prompt('how many sides but less than 20');
+    if (sideNumAsk > sideNum || sideNumAsk < 1) {sideNum = sideNum}
+        else {sideNum = sideNumAsk};
+    play(sideNum);
+});
+document.body.appendChild(btnSide);
 
-for (let i = 0; i < 16; i++) {
-    let newRow = document.createElement('div');
-    for (let j = 0; j < 16; j++) {
-        let newDiv = document.createElement('div');
-        newDiv.style = 'height: 20px; width: 20px; border: solid red 1px';
-        newRow.appendChild(newDiv);
-        newDiv.addEventListener('mouseover', () => {
-            //newDiv.style.borderColor = "blue";
-            newDiv.style.background = "blue";
-        });
-    }
-    document.body.appendChild(newRow);
-}
+function play(sideNum) {
+    console.log('play is working');
+    let playground = document.createElement('div');
+    playground.className = 'playground';
+    for (let i = 0; i < sideNum; i++) {
+        let newRow = document.createElement('div');
+        for (let j = 0; j < sideNum; j++) {
+            let newDiv = document.createElement('div');
+            newDiv.style = 'height: 20px; width: 20px; border: solid red 1px';
+            newRow.appendChild(newDiv);
+            newDiv.addEventListener('mouseover', () => {
+                //newDiv.style.borderColor = "blue";
+                newDiv.style.background = "blue";
+            });
+        };
+        playground.appendChild(newRow);
+    };
+    document.body.appendChild(playground);
+    btnSide.addEventListener('click', () => {
+        document.body.removeChild(playground);
+    });
+};
+
