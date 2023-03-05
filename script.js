835a39ba43e2870686ab27aff3428197ff56f179
pdostonh <67726723+pdostonh@users.noreply.github.com>
2023-03-05 10:39:28 +0500

    randomize sketch color

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ btnSide.innerText = "sides count";
 btnSide.addEventListener('click', () => {
     console.log('buttonFunc is working');
     let sideNumAsk = prompt('how many sides but less than 20');
-    if (sideNumAsk > sideNum || sideNumAsk < 1) {sideNum = 20}
-        else {sideNum = sideNumAsk};
+    if (sideNumAsk > 20 || sideNumAsk < 1) {sideNum = 20}
+    else {sideNum = sideNumAsk};
     play(sideNum);
 });
 document.body.appendChild(btnSide);
@@ -14,15 +14,20 @@ function play(sideNum) {
     console.log('play is working');
     let playground = document.createElement('div');
     playground.className = 'playground';
+    playground.style = 'border: solid red 1px';
+    playground.style.width = (sideNum*22).toString() + 'px';
     for (let i = 0; i < sideNum; i++) {
         let newRow = document.createElement('div');
         for (let j = 0; j < sideNum; j++) {
             let newDiv = document.createElement('div');
             newDiv.style = 'height: 20px; width: 20px; border: solid red 1px';
             newRow.appendChild(newDiv);
+            newDiv.style.background = "#" + Math.floor(Math.random()*16777215).toString(16);
+            let opacity = 0;
+            newDiv.style.opacity = opacity.toString();
             newDiv.addEventListener('mouseover', () => {
-                //newDiv.style.borderColor = "blue";
-                newDiv.style.background = "blue";
+                opacity += 0.1;
+                newDiv.style.opacity = opacity.toString();
             });
         };
         playground.appendChild(newRow);
@@ -32,4 +37,3 @@ function play(sideNum) {
         document.body.removeChild(playground);
     });
 };
-
